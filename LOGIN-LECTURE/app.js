d90a15ec4f2ebbaf3b23bd0fb20378dccf063d6c
youngheon-choi <gjswns21@gmail.com>
2023-03-30 20:24:36 -0700

    express를 쓰는 이유

--- LOGIN-LECTURE/app.js
+++ LOGIN-LECTURE/app.js
@@ -1,5 +1,26 @@
 "use strict";
 
+/*
+express가 없을 때 서버를 여는 방법 
+
+const http = require("http");
+const app =  http.createServer((res, res) => {
+    res.writeHead(200, {
+        "Content-Type" : "text/html; charset=utf-8"
+    });
+
+    if(req.url === "/"){
+        console.log("여기는 루트입니다.");
+    }else if(req.url === "/login"){
+        console.log("여기는 로그인 화면입니다.");
+    }
+});
+
+app.listen(3000, () => {
+    console.log("express없이 서버 여는 방법");
+});
+*/
+
 // "npm i express -s" 후에 express를 가져올 수 있다.
 const express = require("express");
 const app = express();햣 
