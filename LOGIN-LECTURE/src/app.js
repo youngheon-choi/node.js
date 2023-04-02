8548939d0ed1525fe4e948aec61f0b06b6b12329
youngheon-choi <gjswns21@gmail.com>
2023-04-02 13:13:25 -0700

    소스코드 src로 분류

--- LOGIN-LECTURE/src/app.js
+++ LOGIN-LECTURE/src/app.js
@@ -29,15 +29,14 @@ const PORT = 3000;
 const express = require("express");
 const app = express();
 
-// 라우터 연결 = use는 미들웨어를 등록하는 메소드
 // 루트가 전달되면 home변수에 담긴 정보로 이동시킨다(index.js)
 const home = require("./routes/home/index");
-app.use("/", home);
 
 // 앱 세팅
 app.set("views", "./views"); // views에서 화면을 관리한다고 명시합니다.
 app.set("view engine", "ejs"); // view(html)코드를 어떤 엔진으로 해석할지 명시합니다.
 
-app.listen(PORT, () => {
-    console.log("서버 가동");
-});
+// 라우터 연결 = use는 미들웨어를 등록하는 메소드
+app.use("/", home);
+
+module.exports = app;
